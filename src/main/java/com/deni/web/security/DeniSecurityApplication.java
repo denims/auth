e59d5fad8525f2436986c989c2777f8ad4308684
Deni Simon <denimallel@gmail.com>
2019-08-18 21:10:54 +0530

    added basic auth

---
+++ src/main/java/com/deni/web/security/DeniSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.deni.web.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeniSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeniSecurityApplication.class, args);
+    }
+
+}
